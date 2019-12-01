477cbd06ece82e613f0b80daf12b7eaf7bd3582f
pccisme <pccisme@gmail.com>
2019-12-01 17:50:25 -0600

    Update pointScreen.js

--- views/pointScreen.js
+++ views/pointScreen.js
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent:'center',
-    backgroundColor:'#eeeeee',
+    // backgroundColor:'#eeeeee',
     // borderColor: 'orange',
     // borderWidth: 2,
   },
